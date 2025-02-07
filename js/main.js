04cc4a7d6bd67a955e29e2eb57264571eb96244e
Natnael Dejene <thecodegeek1@gmail.com>
2025-02-07 05:54:13 +0300

    make the faq interactive

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//FAQ accordion 
+document.addEventListener("DOMContentLoaded", () => {
+    const faqContainer = document.querySelector('.faq-content');
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.faq-group-header');
+
+        if (!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.faq-group-body');
+        const icon = group.querySelector('i');
+
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        groupBody.classList.toggle('open');
+
+        const otherGroups = document.querySelectorAll('.faq-group');
+        otherGroups.forEach((otherGroup) => {
+
+            if (otherGroup != group) {
+                const otherBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+                otherBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+            }
+
+        })
+
+    })
+})
